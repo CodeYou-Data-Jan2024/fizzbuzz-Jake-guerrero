1ceb27c855c9852e8065f558e4be8736276a0941
Jake Guerrero <jakeguerrero9@aol.com>
2024-03-30 10:01:41 -0400

    adding fizzbuzz code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,5 @@
-# add your code here
 
-target = 100
-for number in range(1, target + 1):
+for number in range(1, 101):
   if number % 3 == 0 and number % 5 == 0:
     print("FizzBuzz")
   elif number % 3 == 0:
